366fefc056bfb7f3e3f188f8ebb5a196941b9436
Manuel Muñoz Solera <mamuso@mamuso.net>
2016-11-04 00:42:55 -0700

    starting to fill the points

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'cairo'
 gem 'numo-narray'
+gem 'distribution'
--- hyphae.rb
+++ hyphae.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'cairo'
 require 'numo/narray'
+require 'distribution'
 # require ''
 
 # Configuration
@@ -24,13 +25,13 @@ ZONES = (SIZE/ZONEWIDTH).to_i
 # filename = 'generations_a'
 # DRAW_SKIP = 10000 # write image this often
 
-# RAD_SCALE = 0.92
-# SEARCH_ANGLE_MAX = pi
+RAD_SCALE = 0.92 # CHECK
+SEARCH_ANGLE_MAX = Math::PI
 
 # R_RAND_SIZE = 6
-# CK_MAX = 15 # max number of allowed branch attempts from a node
+CK_MAX = 15 # max number of allowed branch attempts from a node
 
-# CIRCLE_RADIUS = 0.45
+CIRCLE_RADIUS = 0.45
 
 SOURCE_NUM = 9
 
@@ -65,7 +66,6 @@ class Hyphae
   end 
 
   def generate
-    # puts ((ZONES+2)**2) # why +2? CHECK 
     cz = []
     ((ZONES+2)**2).times { cz << [] }
 
@@ -76,7 +76,7 @@ class Hyphae
     cge = Numo::DFloat.zeros(NMAX) # CHECK
     cp = Numo::DFloat.zeros(NMAX) # PARENT
     cc = Numo::DFloat.zeros(NMAX) # CHECK
-    cd = Numo::DFloat.zeros(NMAX) - 1 # CHECK
+    cd = Numo::DFloat.zeros(NMAX).fill(-1)
 
     ## number of nodes
     i = 0
@@ -100,10 +100,58 @@ class Hyphae
       cp[i] = -1 # NO PARENT
       # cr[i] = RAD
       z = get_z(cx[i], cy[i])
-      puts z
       cz[z].push(i)
       
       i += 1
+    end
+
+    num = i
+    itt = 0
+    ti = Time.now
+    drawn = -1
+
+
+    while true
+      begin
+        itt += 1
+        if itt%1000 == 0
+          # puts ("#{itt}, #{num}")
+        end
+        added_new = false
+
+        k = (rand * num).to_i
+        cc[k] += 1
+        if cc[k] > CK_MAX
+          next
+        end
+
+        r = (cd[k] > -1) ? cr[k] * RAD_SCALE : cr[k]
+        if r < ONE
+          ## node dies
+          cc[k] = CK_MAX+1
+          next
+        end
+
+        ge = (cd[k] > -1) ? cge[k]+1 : cge[k]
+        the = cthe[k] + (1 - 1/( (ge+1)**0.1) ) * Distribution::Normal.pdf(rand) * SEARCH_ANGLE_MAX
+
+        x = cx[k] + Math.sin(the)*r
+        y = cy[k] + Math.cos(the)*r
+
+        circle_rad = sqrt((x-0.5)**2 + square(y-0.5)**2)
+        if circle_rad > CIRCLE_RADIUS:
+          ## node is outside circle
+          next
+        end
+      
+
+        num += 1
+        added_new = true
+
+
+      rescue
+        redo # run the current iteration again
+      end
     end
 
   end
